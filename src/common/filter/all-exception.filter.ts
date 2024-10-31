925d4ca5af417b577009de38a2e9606191621556
imronshoimov <imronshoimov125@gmail.com>
2024-10-31 11:43:57 +0500

    fixed filter exception for prisma errors

--- src/common/filter/all-exception.filter.ts
+++ src/common/filter/all-exception.filter.ts
@@ -5,34 +5,28 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
+import { Prisma } from '@prisma/client';
 import { Response } from 'express';
 
 @Catch()
 export class AllExceptionFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
-    console.log(exception, 'EXCETPTION');
-
     const contextType = host.getType<'rmq' | 'http'>();
-    const code = exception?.response?.statusCode ?? 500;
-    const error = exception?.response?.message ?? 'Internal server error';
-    console.log(exception?.meta, 'E filter');
+    let code = exception?.response?.statusCode ?? 500;
+    let error = exception?.response?.message ?? 'Internal server error';
 
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
 
-    let prismaError: string;
-    if (exception?.meta) {
-      prismaError = `${exception?.meta?.cause} ${exception?.meta?.modelName}`;
-    }
-
-    let prismaStatusCode: number;
-    if (exception?.code == 'P2025') {
-      prismaStatusCode = HttpStatus.NOT_FOUND;
+    // Prisma error handling block
+    if (exception instanceof Prisma.PrismaClientKnownRequestError) {
+      code = HttpStatus.BAD_REQUEST
+      error = `${exception.meta.modelName}, ${exception.meta.cause}`
     }
 
     const body = {
-      code: prismaStatusCode || code,
-      error: prismaError || error,
+      code,
+      error,
     };
 
     if (contextType === 'http') {
