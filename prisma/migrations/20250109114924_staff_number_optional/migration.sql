cec5e5493704d0cb079e7bdb107f08b42b744b36
imron <imronshoimov125@gmail.com>
2025-01-09 16:50:17 +0500

    changed staffNumber to optional

---
+++ prisma/migrations/20250109114924_staff_number_optional/migration.sql
@@ -0,0 +1,56 @@
+-- AlterTable
+ALTER TABLE "area" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "avenue" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "category" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "district" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "impasse" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "lane" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "main_organization" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "nearby" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "nearby_category" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "organization" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "organization_version" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "passage" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "phone_types" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "product_service_category" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "product_service_sub_category" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "residential_area" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "street" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "sub_category" ALTER COLUMN "staff_number" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "village" ALTER COLUMN "staff_number" DROP NOT NULL;
