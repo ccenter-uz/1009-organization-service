f348e5760ad378979f168aaa54d03c15b7dcbcc6
elshod tukhtamurodov <elshodweb@gmail.com>
2025-02-03 18:12:43 +0500

    refactor: remove additional content and table modules and update related database schema

---
+++ src/common/helper/sql-rows-for-select/get-ordered-additional-data.dto.ts
@@ -0,0 +1,118 @@
+import { PrismaService } from '@/modules/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
+
+export async function getAllAdditional(
+  prisma: PrismaService,
+  data: any,
+  pagination?: { take: number; skip: number }
+) {
+  const conditions: Prisma.Sql[] = [];
+  if (data.status === 0 || data.status === 1)
+    conditions.push(Prisma.sql`a.status = ${data.status}`);
+
+  if (data.search) {
+    conditions.push(Prisma.sql`
+      EXISTS (
+        SELECT 1
+        FROM additional_translations at
+        WHERE at.additional_id = a.id
+          AND at.language_code = ${data.langCode}
+          AND at.name ILIKE ${`%${data.search}%`}
+      )
+    `);
+  }
+
+  const whereClause =
+    conditions.length > 0
+      ? Prisma.sql`WHERE ${Prisma.join(conditions, ' AND ')}`
+      : Prisma.empty;
+
+  const result: any = await prisma.$queryRaw(
+    Prisma.sql`
+      WITH
+        AdditionalTranslations AS (
+          SELECT
+            at.additional_id,
+            JSON_AGG(
+              JSONB_BUILD_OBJECT(
+                'languageCode', at.language_code,
+                'name', at.name
+              )
+            )::JSONB AS AdditionalTranslations  
+          FROM additional_translations at
+          WHERE (${data.allLang} = TRUE OR at.language_code = ${data.langCode})
+          GROUP BY at.additional_id
+        ),
+        AdditionalWarningTranslations AS (
+          SELECT
+            awt.additional_id,
+            JSON_AGG(
+              JSONB_BUILD_OBJECT(
+                'languageCode', awt.language_code,
+                'name', awt.name
+              )
+            )::JSONB AS AdditionalWarningTranslations  
+          FROM additional_warning_translations awt
+          WHERE (${data.allLang} = TRUE OR awt.language_code = ${data.langCode})
+          GROUP BY awt.additional_id
+        ),
+        AdditionalMentionTranslations AS (
+          SELECT
+            amt.additional_id,
+            JSON_AGG(
+              JSONB_BUILD_OBJECT(
+                'languageCode', amt.language_code,
+                'name', amt.name
+              )
+            )::JSONB AS AdditionalMentionTranslations  
+          FROM additional_mention_translations amt
+          WHERE (${data.allLang} = TRUE OR amt.language_code = ${data.langCode})
+          GROUP BY amt.additional_id
+        ),
+        AdditionalContent AS (
+          SELECT
+            ac.additional_id,
+            JSON_AGG(
+              JSONB_BUILD_OBJECT(
+                'id', ac.id,
+                'status', ac.status,
+                'createdAt', ac.created_at,
+                'updatedAt', ac.updated_at,
+                'deletedAt', ac.deleted_at
+              )
+            )::JSONB AS AdditionalContent
+          FROM additional_content ac
+          GROUP BY ac.additional_id
+        ),
+        AdditionalTable AS (
+          SELECT
+            atb.additional_id,
+            JSON_AGG(
+              JSONB_BUILD_OBJECT(
+                'id', atb.id,
+                'status', atb.status,
+                'createdAt', atb.created_at,
+                'updatedAt', atb.updated_at,
+                'deletedAt', atb.deleted_at
+              )
+            )::JSONB AS AdditionalTable
+          FROM additional_table atb
+          GROUP BY atb.additional_id
+        )
+      SELECT
+        a.*,
+        (SELECT AdditionalTranslations FROM AdditionalTranslations WHERE additional_id = a.id) AS "AdditionalTranslations",
+        (SELECT AdditionalWarningTranslations FROM AdditionalWarningTranslations WHERE additional_id = a.id) AS "AdditionalWarningTranslations",
+        (SELECT AdditionalMentionTranslations FROM AdditionalMentionTranslations WHERE additional_id = a.id) AS "AdditionalMentionTranslations",
+        (SELECT AdditionalContent FROM AdditionalContent WHERE additional_id = a.id) AS "AdditionalContent",
+        (SELECT AdditionalTable FROM AdditionalTable WHERE additional_id = a.id) AS "AdditionalTable"
+      FROM
+        additional a
+      ${whereClause}
+      GROUP BY a.id
+      ${pagination ? Prisma.sql`LIMIT ${pagination.take} OFFSET ${pagination.skip}` : Prisma.empty}
+    `
+  );
+
+  return result;
+}
