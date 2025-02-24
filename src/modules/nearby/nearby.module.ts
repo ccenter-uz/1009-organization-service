0427ba1fa657ebed59da8c03edd007a8823734e0
elshod tukhtamurodov <elshodweb@gmail.com>
2025-02-24 13:59:16 +0500

    Add district relation to Nearby model and update NearbyService to handle district data

--- src/modules/nearby/nearby.module.ts
+++ src/modules/nearby/nearby.module.ts
@@ -4,9 +4,10 @@ import { NearbyService } from './nearby.service';
 import { CityModule } from '../city/city.module';
 import { RegionModule } from '../region/region.module';
 import { NearbyCategoryModule } from '../nearby-category/nearby-category.module';
+import { DistrictModule } from '../district/district.module';
 
 @Module({
-  imports: [CityModule, RegionModule, NearbyCategoryModule],
+  imports: [CityModule, RegionModule, NearbyCategoryModule,DistrictModule],
   controllers: [NearbyController],
   providers: [NearbyService],
   exports: [NearbyService],
